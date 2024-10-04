07accf758f3ef0841f24526e8237cc2aeb28b185
nicholelouis <nicholelouiss@gmail.com>
2024-10-04 15:07:47 +0100

    arreglos spotify

--- spotify/js/song.js
+++ spotify/js/song.js
@@ -2,21 +2,22 @@ export default class Song {
     constructor(k_song, v_song, c_song) {
         this.element = document.querySelector(k_song);  
         this.audio = new Audio(v_song);  
-        this.album = document.querySelector(c_song);  
+        this.album = document.querySelector(c_song); 
     }
 }
 
 export function play_song(song) {
-    
+    // Referencia al disco (vinyl) correspondiente al contenedor (item) de la canción
     const disc = song.element.querySelector('.vinyl');
 
     song.element.onclick = () => {
         if (song.audio.paused) {
-            song.audio.play();  
-            disc.classList.add('visible');  
+            song.audio.play();  // Reproduce la canción
+            disc.classList.add('visible');  // Muestra el disco
         } else {
-            song.audio.pause();  
-            disc.classList.remove('visible');  
+            song.audio.pause();  // Pausa la canción
+            disc.classList.remove('visible');  // Oculta el disco
         }
     };
 }
+
